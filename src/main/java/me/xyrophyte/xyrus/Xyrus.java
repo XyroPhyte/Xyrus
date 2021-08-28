07a08241690956dfdbfc3fa78f2fe0705d420a6c
xyrophyte <emadharun.eh@gmail.com>
2021-08-28 12:27:30 +0600

    Logging to console on startup & shutdown

--- src/main/java/me/xyrophyte/xyrus/Xyrus.java
+++ src/main/java/me/xyrophyte/xyrus/Xyrus.java
@@ -1,5 +1,6 @@
 package me.xyrophyte.xyrus;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Xyrus extends JavaPlugin {
@@ -7,11 +8,18 @@ public final class Xyrus extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info(ChatColor.BLUE + "Plugin is starting up");
+        getLogger().info(ChatColor.AQUA + "by XyroPhyte");
+
+        getLogger().info(ChatColor.BLUE + "Plugin started successfully");
+        getLogger().info(ChatColor.AQUA + "For support, join:");
+        getLogger().info(ChatColor.GOLD + "https://discord.gg/dpYrXXWWrM");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Plugin is shutting down");
     }
 }
